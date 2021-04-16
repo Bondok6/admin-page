41f0fad26941d434c64abe694080b44340fa7a06
Bondok6 <kirolousbondok6@gmail.com>
2021-04-16 21:19:38 +0200

    admin page

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
+// Select options
+
+const days = document.getElementById('days');
+const years = document.getElementById('years');
+const months = document.getElementById('months');
+
+for (let day = 1; day <= 30; day++) {
+	days.innerHTML += `<option value="1"> ${day}</option>`;
+}
+for (let year = 1990; year <= 2021; year++) {
+	years.innerHTML += `<option value="1"> ${year}</option>`;
+}
+
+const theMonths = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+
+for (let month = 0; month < theMonths.length; month++) {
+	months.innerHTML += `<option value="1"> ${theMonths[month]}</option>`;
+}
+
+// side bar buttons
 const addImg = document.querySelector('.add__img');
 const graphImg = document.querySelector('.graph__img');
 
